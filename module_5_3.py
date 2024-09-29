67a6c3033809082a9c91a4925ff31ea040fde6ee
Palina <polina.sofina.20@mail.ru>
2024-09-29 10:20:42 +0500

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -25,47 +25,39 @@ class House:
     def __eq__(self, other):
         if isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
-        return NotImplemented
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
     def __lt__(self, other):
         if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
-        return NotImplemented
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors <= other.number_of_floors
-        return NotImplemented
+        return self.__eq__(other) or self.__lt__(other)
 
     def __gt__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors > other.number_of_floors
-        return NotImplemented
+        return not self.__le__(other)
 
     def __ge__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors <= other.number_of_floors
-        return NotImplemented
+        return not self.__lt__(other)
 
     def __ne__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors != other.number_of_floors
-        return NotImplemented
+        return not self.__eq__(other)
 
     def __add__(self, value):
         if isinstance(value, int):
-            return self.number_of_floors + value
-        return NotImplemented
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        return self
 
-    def __radd__(self, value):
-        if isinstance(value, int):
-            return self.number_of_floors + value
-        return NotImplemented
+    def __radd__(self, other):
+        return self.__add__(other)
 
-    def __iadd__(self, value):
-        if isinstance(value, int):
-            return self.number_of_floors + value
-        return NotImplemented
+    def __iadd__(self, other: int):
+        return self.__add__(other)
 
 
 
